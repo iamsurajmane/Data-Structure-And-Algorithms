82230bf397d9fce15fb277ba3f6e37baceecd579
Suraj <sm3031972@gmail.com>
2026-09-25 21:39:18 +0530

    linear search

---
+++ Array/LinearSearch.java
@@ -0,0 +1,16 @@
+public class LinearSearch {
+    public static boolean search(int arr[] , int k){
+        for(int i=0;i<arr.length;i++){
+            if(arr[i] == k){
+                return true;
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        int arr[] = {22,34,54,32,21,24,56};
+        int k = 84;
+
+        System.out.println(search(arr, k));
+    }
+}
