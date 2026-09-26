8e76b3b67538fb14574db4ee3c1a769b04969c90
Suraj <sm3031972@gmail.com>
2026-09-26 12:25:53 +0530

    method 1

---
+++ Array/FindNumberAppearingOnce.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+
+public class FindNumberAppearingOnce {
+    public static int method1(int arr[]){
+        HashMap<Integer,Integer> map = new HashMap<>();
+
+        for(int num : arr){
+            if(map.containsKey(num)){
+                map.put(num, map.get(num)+1);
+            }else{
+                map.put(num, 1);
+            }
+        }
+
+        for(int num : arr){
+            if(map.get(num) == 1)  return num;
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int arr[] = { 2,3,4,5,4,5,3,2,1,1,9};
+
+        System.out.println(method1(arr));
+    }
+}
