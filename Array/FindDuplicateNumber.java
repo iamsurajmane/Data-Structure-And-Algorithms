d9477bac6445fe4960ef9f3ebc780ed914f99ee3
Suraj <sm3031972@gmail.com>
2026-09-26 10:52:10 +0530

    method 2

--- Array/FindDuplicateNumber.java
+++ Array/FindDuplicateNumber.java
@@ -1,23 +1,45 @@
+import java.util.Arrays;
 import java.util.HashSet;
 
 public class FindDuplicateNumber {
 
-    METHOD  - 1
+    // METHOD  - 1
 
-    public static void findDuplicate1(int arr[]){
-        HashSet<Integer> set = new HashSet<>();
+    //  this method uses extra space
+    // time complexity - O(n)
 
-        for(int num : arr){
-            if(set.contains(num)){
-                System.out.println("Duplicate Number Found : "+num);
+    // public static void findDuplicate1(int arr[]){
+    //     HashSet<Integer> set = new HashSet<>();
+
+    //     for(int num : arr){
+    //         if(set.contains(num)){
+    //             System.out.println("Duplicate Number Found : "+num);
+    //             return;
+    //         }
+    //         set.add(num);
+    //     }
+    // }
+
+
+    // METHOD - 2
+
+    public static void findDuplicate2(int arr[]){
+        Arrays.sort(arr);
+
+        for(int i=1;i<arr.length;i++){
+            if(arr[i] == arr[i-1]){
+                System.out.println("Duplicate Number Found : "+arr[i]);
                 return;
             }
-            set.add(num);
         }
     }
     public static void main(String[] args) {
-        int arr[] = {1,4,3,2,3};
+        int arr[] = {1,4,3,2,4};
 
-        findDuplicate1(arr);
+        // METHOD - 1
+        // findDuplicate1(arr);
+
+        // METHOD - 2
+        findDuplicate2(arr);
     }
 }
