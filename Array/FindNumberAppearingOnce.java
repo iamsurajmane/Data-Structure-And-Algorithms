921c43992cd415788eab16f0c099ca1f7da3d062
Suraj <sm3031972@gmail.com>
2026-09-26 12:27:29 +0530

    comments

--- Array/FindNumberAppearingOnce.java
+++ Array/FindNumberAppearingOnce.java
@@ -1,25 +1,32 @@
 import java.util.HashMap;
 
 public class FindNumberAppearingOnce {
-    public static int method1(int arr[]){
-        HashMap<Integer,Integer> map = new HashMap<>();
 
-        for(int num : arr){
-            if(map.containsKey(num)){
-                map.put(num, map.get(num)+1);
-            }else{
-                map.put(num, 1);
-            }
-        }
+    // Method -- 1
+    // Time Complexity - O(n)
 
-        for(int num : arr){
-            if(map.get(num) == 1)  return num;
-        }
-        return -1;
-    }
+    // public static int method1(int arr[]){
+    //     HashMap<Integer,Integer> map = new HashMap<>();
+
+    //     for(int num : arr){
+    //         if(map.containsKey(num)){
+    //             map.put(num, map.get(num)+1);
+    //         }else{
+    //             map.put(num, 1);
+    //         }
+    //     }
+
+    //     for(int num : arr){
+    //         if(map.get(num) == 1)  return num;
+    //     }
+    //     return -1;
+    // }
     public static void main(String[] args) {
         int arr[] = { 2,3,4,5,4,5,3,2,1,1,9};
 
-        System.out.println(method1(arr));
+        // METHOD -- 1
+        // System.out.println(method1(arr));
+
+
     }
 }
